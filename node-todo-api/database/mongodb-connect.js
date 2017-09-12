f7cdcdbe4ec6c152fea1b546b2c10e49d232fbf6
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-12 20:12:41 +1200

    creating a new file for fetching data

--- node-todo-api/database/mongodb-connect.js
+++ node-todo-api/database/mongodb-connect.js
@@ -1,35 +1,32 @@
 // const MongoClient = require('mongodb').MongoClient
 const {MongoClient, ObjectID} = require('mongodb')
 
-var obj = new ObjectID()
-console.log(obj)
-
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to Mongodb server')
   }
   console.log('Connect to mongodb server')
 
-  // db.collection('Todos').insertOne({
-  //   text: 'Something to add',
-  //   completed: false
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert data')
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2))
-  // })
+  db.collection('Todos').insertOne({
+    text: 'Something to add',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert data')
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2))
+  })
 
-  // db.collection('Users').insertOne({
-  //   name: 'Ali Kahwaji',
-  //   age: 37,
-  //   location: 'Auckland'
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert users data', err)
-  //   }
-  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp()))
-  // })
+  db.collection('Users').insertOne({
+    name: 'Ali Kahwaji',
+    age: 37,
+    location: 'Auckland'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert users data', err)
+    }
+    console.log(JSON.stringify(result.ops[0]._id.getTimestamp()))
+  })
 
   db.close()
 })
---
+++ node-todo-api/database/mongodb-find.js
@@ -0,0 +1,12 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to Mongodb server')
+  }
+  console.log('Connect to mongodb server')
+  
+  db.collection('Todos').find()
+  db.close()
+})
