9073c436e94acde7dfb202c16eba09283ebeb8c7
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-12 22:00:24 +1200

    find and fetching a specific data

--- node-todo-api/database/mongodb-find.js
+++ node-todo-api/database/mongodb-find.js
@@ -6,7 +6,24 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     return console.log('Unable to connect to Mongodb server')
   }
   console.log('Connect to mongodb server')
-  
-  db.collection('Todos').find()
-  db.close()
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('59b7968db904c0aac11cbe90')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos')
+  //   console.log(JSON.stringify(docs, undefined, 2))
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // })
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`)
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // })
+
+  db.collection('Todos').find({text: 'Name to add'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2))
+  })
+ // db.close()
 })
