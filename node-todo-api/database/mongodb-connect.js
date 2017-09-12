47ba2d2588f4c9e985c24dc7e6e5f91c39e5ce20
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-12 19:36:12 +1200

    inserting a new collection

--- node-todo-api/database/mongodb-connect.js
+++ node-todo-api/database/mongodb-connect.js
@@ -6,5 +6,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connect to mongodb server')
 
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to add',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert data')
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2))
+  // })
+
+  db.collection('Users').insertOne({
+    name: 'Ali Kahwaji',
+    age: 37,
+    location: 'Auckland'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert users data', err)
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2))
+  })
+
   db.close()
 })
