30bc22db19962237ac7a1a44d17152e443b4f57e
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-12 15:01:35 +1200

    Setting mongodb connection

--- node-todo-api/database/mongodb-connect.js
+++ node-todo-api/database/mongodb-connect.js
@@ -1,3 +1,10 @@
 const MongoClient = require('mongodb').MongoClient
 
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to Mongodb server')
+  }
+  console.log('Connect to mongodb server')
 
+  db.close()
+})
