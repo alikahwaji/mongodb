652fe0d39b095b20cad1905ca86419644aa74fd2
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-12 20:05:14 +1200

    Creating a destrcture code

--- node-todo-api/database/mongodb-connect.js
+++ node-todo-api/database/mongodb-connect.js
@@ -1,4 +1,8 @@
-const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+var obj = new ObjectID()
+console.log(obj)
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
@@ -6,26 +10,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connect to mongodb server')
 
-  db.collection('Todos').insertOne({
-    text: 'Something to add',
-    completed: false
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert data')
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2))
-  })
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to add',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert data')
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2))
+  // })
 
-  db.collection('Users').insertOne({
-    name: 'Ali Kahwaji',
-    age: 37,
-    location: 'Auckland'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert users data', err)
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2))
-  })
+  // db.collection('Users').insertOne({
+  //   name: 'Ali Kahwaji',
+  //   age: 37,
+  //   location: 'Auckland'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert users data', err)
+  //   }
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp()))
+  // })
 
   db.close()
 })
