bfdc06c70f9d04f5b235b5017899775e73b231f0
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-13 10:44:48 +1200

    Deleting inserted data from collection

--- node-todo-api/database/mongodb-delete.js
+++ node-todo-api/database/mongodb-delete.js
@@ -7,5 +7,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connect to mongodb server')
 
+  db.collection('Todos').deleteMany({text: 'Title to add'}).then((result) => {
+    console.log(result)
+  })
+
+  db.collection('Todos').deleteOne({text: 'Name to add'}).then((result) => {
+    console.log(result)
+  })
+
+  db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    console.log(result)
+  })
+
  // db.close()
 })
