79017ed5520891020909ac33efa4b9e3f484c24a
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-13 12:31:01 +1200

    Updating users collection with inc the age prop

--- node-todo-api/database/mongodb-update.js
+++ node-todo-api/database/mongodb-update.js
@@ -6,26 +6,41 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     return console.log('Unable to connect to Mongodb server')
   }
   console.log('Connect to mongodb server')
-  db.collection('Todos').findOneAndUpdate({
-    _id: new ObjectID('59b7968db904c0aac11cbe90')
-  }, {
-    $set: {
-      completed: false
-    }
-  }, {
-    returnOriginal: true
-  }).then((result) => {
-    console.log(result)
-  })
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('59b7968db904c0aac11cbe90')
+  // }, {
+  //   $set: {
+  //     completed: false
+  //   }
+  // }, {
+  //   returnOriginal: true
+  // }).then((result) => {
+  //   console.log(result)
+  // })
 
-  db.collection('Todos').findOneAndUpdate({
-    _id: new ObjectID('59b79274e41035723033d322')
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('59b79274e41035723033d322')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result)
+  // })
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('59b86aaeb904c0aac11cc3a0')
   }, {
     $set: {
-      completed: true
+      name: 'Ali Kahwaji'
+    },
+    $inc: {
+      age: 8
     }
   }, {
-    returnOriginal: false
+    returnOriginal: 'Mohammed Kahwaji'
   }).then((result) => {
     console.log(result)
   })
