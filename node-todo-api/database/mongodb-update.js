f27a8de20756b4c01b41a70a9652a8c5ffe51454
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-13 12:20:03 +1200

    Updating other collection

--- node-todo-api/database/mongodb-update.js
+++ node-todo-api/database/mongodb-update.js
@@ -6,8 +6,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     return console.log('Unable to connect to Mongodb server')
   }
   console.log('Connect to mongodb server')
-  db.colection('Todos').findOneAndUpdate({
+  db.collection('Todos').findOneAndUpdate({
     _id: new ObjectID('59b7968db904c0aac11cbe90')
+  }, {
+    $set: {
+      completed: false
+    }
+  }, {
+    returnOriginal: true
+  }).then((result) => {
+    console.log(result)
+  })
+
+  db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectID('59b79274e41035723033d322')
   }, {
     $set: {
       completed: true
