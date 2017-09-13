9f7c2bbe64270a317664c07172d875fcc9923496
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-13 12:11:18 +1200

    Updating insrted data

---
+++ node-todo-api/database/mongodb-update.js
@@ -0,0 +1,13 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to Mongodb server')
+  }
+  console.log('Connect to mongodb server')
+  db.colection('Todos').findOneAndUpdate({
+    _id: new ObjectID('59b7968db904c0aac11cbe90')
+  })
+  // db.close()
+})
